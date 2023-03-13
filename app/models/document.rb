3c51b8a911bb3abc24380b9c335a25761d1c84fb
nsaini007 <nsaini@gammastack.com>
2023-03-13 15:49:50 +0530

    commit 5

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,6 @@
 class Document < ApplicationRecord
+  DOC_TYPES = ["Marksheet", "Photo ID", "Address Proof", "Others"].freeze
+
   belongs_to :employee
 
   validates :name, :doc_type , :employee_id, presence: true
